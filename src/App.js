fba1d1c183128cfc287857555e76f91293b54d99
Carol Wang <xtwang0@uw.edu>
2021-04-29 03:29:12 -0700

    clean data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from "react";
+import { useFetch } from "./hooks/useFetch";
+import { scaleLinear}  from "d3-scale";
+import { extent, max, min, bin } from "d3-array";
+
 
 const App = () => {
+
+    const [data, loading] = useFetch(
+        "https://raw.githubusercontent.com/annie2980/info474-annie2980/main/world-happiness-report-2021.csv"
+    );
+
     return (
     <div>
-        <h2>la Scores</h2>
-        <p>april</p>
+        <h1>Exploratory Data Analysis Focus on World Population</h1>
     </div>
     );
 };
+
 
 export default App;
