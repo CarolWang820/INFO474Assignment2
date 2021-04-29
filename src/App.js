2b1f421f004a2a8c0ffc95e25ed4d3ef1e849bb9
Carol Wang <xtwang0@uw.edu>
2021-04-29 03:59:21 -0700

    new data

--- src/App.js
+++ src/App.js
@@ -5,14 +5,14 @@ import { extent, max, min, bin } from "d3-array";
 
 
 const App = () => {
-
     const [data, loading] = useFetch(
-        "https://raw.githubusercontent.com/annie2980/info474-annie2980/main/world-happiness-report-2021.csv"
+        "https://raw.githubusercontent.com/CarolWang820/INFO474Assignment2/main/countries%20of%20the%20world.csv"
     );
 
     return (
     <div>
         <h1>Exploratory Data Analysis Focus on World Population</h1>
+
     </div>
     );
 };
