ab49e5f8b7d20d787d1cea83390578fd50e2b1ba
Carol Wang <xtwang0@uw.edu>
2021-04-30 08:40:26 -0700

    finished A2 add feedback

--- src/App.js
+++ src/App.js
@@ -34,11 +34,18 @@ const App = () => {
     <div>
         <h1>Exploratory Data Analysis Focus on Vedio Game Sales</h1>
         <p>{loading && "Loading data!"}</p>
-        <h2>Introduction</h2>
+        <h2>Paragraph 1: Introduction</h2>
+        <p>The dataset I use is Video Game Sales (Analyze sales data from more than 16,500 games) from <a href="https://www.kaggle.com/gregorut/videogamesales">Kaggle</a>. This dataset is pretty straight forwards, and it doesn't contain any null data. It shows the video games' sales amount based on the regions. There are five different regions in this dataset which are North America, Europe, Japan, Sales in the rest of the world,  Total worldwide sales. For each video game, it also provides the information of its platform of release, year of release, publisher. So there are 8 factors. Since the data file is too large, I picked the top 300 video games when I'm analyzing. Based on the factors, the relationship between video games' features and their sales amount in different regions became the main point I want to explore. And video game companies can also look for the trend in different regions for them to make better business decision. </p>
         
-        <h3>1. </h3>
-        <h4>1.1 na sale</h4>
-
+        <h2>Paragraph 2: The four questions I want to address:</h2>
+        <ol>
+            <li>What are the distributions of sales (in millions) for the video game in North America, Europe, and Japan?  </li>
+            <li>What are the distributions of sales (in millions) for the video game that has DS, PS2 platform in North America compared with all the video platforms' games in North America?</li>
+            <li>What are the distributions of sales (in millions) for the video game of Genre sports in EU and JP compared with all the Genre games in these countries?</li>
+            <li>What are the distributions of sales (in millions) for video games in the Year 2002 compared to the global game's sales in the all-time period(1980-2020)?</li>
+        </ol>
+        <h3>1. What are the distributions of sales (in millions) for the video game in North America, Europe, and Japan?  </h3>
+        <h4>1.1 Distributions of Video Games' sales (in millions) in North America</h4>
         <svg width={size} height={size} style={{border: "1px solid black"}}>
             <text x={size / 2 - 20} textAnchor="end" y={size - margin +3} 
             style={{fontSize: 10, fontFamily: "italic"}}
@@ -77,9 +84,10 @@ const App = () => {
                 );
             })}
         </svg>
+        <p>This barcode plot shows the distributions of video games' sales (in millions) in North America. The maximum sales amount (in millions) is 41.49 and the minimum sales amount (in millions) is 0. The distribution is skewed to the right. </p>
 
-        <h4>1.2 eu sale</h4>
 
+        <h4>1.2 Distributions of Video Games' sales (in millions) in Europe</h4>
         <svg width={size} height={size} style={{border: "1px solid black"}}>
             <text x={size / 2 - 20} textAnchor="end" y={size - margin +3} 
             style={{fontSize: 10, fontFamily: "italic"}}
@@ -118,10 +126,11 @@ const App = () => {
                 );
             })}
         </svg>
+        <p>This barcode plot shows the distributions of video games' sales (in millions) in Europe. The maximum sales amount (in millions) is 29.02 and the minimum sales amount (in millions) is 0. The distribution is skewed to the right. </p>
 
 
-        <h4>1.3 jp sale</h4>
 
+        <h4>1.3 Distributions of Video Games' sales (in millions) in Japan</h4>
         <svg width={size} height={size} style={{border: "1px solid black"}}>
             <text x={size / 2 - 20} textAnchor="end" y={size - margin +3} 
             style={{fontSize: 10, fontFamily: "italic"}}
@@ -160,11 +169,12 @@ const App = () => {
                 );
             })}
         </svg>
+        <p>This barcode plot shows the distributions of video games' sales (in millions) in Japan. The maximum sales amount (in millions) is 10.22 and the minimum sales amount (in millions) is 0. The distribution is skewed to the right. </p>
 
 
-        <h3>2. platform on na</h3>
-        <h4>2.1 ds</h4>
 
+        <h3>2. What are the distributions of sales (in millions) for the video game that has DS, PS2 platform in North America compared with all the video platforms' games in North America?</h3>
+        <h4>2.1 Highlighted Distributions of DS platform's Video Games' sales (in millions) in North America</h4>
         <svg width={size} height={size} style={{border: "1px solid black"}}>
             <text x={size / 2 - 20} textAnchor="end" y={size - margin +3} 
             style={{fontSize: 10, fontFamily: "italic"}}
@@ -226,10 +236,11 @@ const App = () => {
                 );
             })}
         </svg>
+        <p>This graph shows the comparison between DS platform's Video Games' sales (red) and all platform's Video Games' sales(blue) in North America. The maximum sales amount of DS platform's Video Games (in millions) is 11.32 and the minimum sales amount (in millions) is 0.3. The graph shows that the distribution of DS platform is similar with all platforms. </p>
 
 
-        <h4>2.2 ps2</h4>
 
+        <h4>2.2 Highlighted Distributions of PS2 platform's Video Games' sales (in millions) in North America</h4>
         <svg width={size} height={size} style={{border: "1px solid black"}}>
         <text x={size / 2 - 20} textAnchor="end" y={size - margin +3} 
             style={{fontSize: 10, fontFamily: "italic"}}
@@ -291,12 +302,13 @@ const App = () => {
                 );
             })}
         </svg>
+        <p>This graph shows the comparison between PS2 platform's Video Games' sales (red) and all platform's Video Games' sales(blue) in North America. The maximum sales amount of DS platform's Video Games (in millions) is 17.52 and the minimum sales amount (in millions) is 0.1. The graph shows that the distribution of PS2 platform is similar with all platforms. </p>
 
 
 
-        <h3>3. genre on eu</h3>
-        <h4>3.1 sports</h4>
 
+        <h3>3. What are the distributions of sales (in millions) for the video game of Genre sports in EU and JP compared with all the Genre games in these countries?</h3>
+        <h4>3.1 Highlighted Distributions of Sports genre Video Games' sales (in millions) in Europe </h4>
         <svg width={size} height={size} style={{border: "1px solid black"}}>
             
             <text x={size / 2 - 20} textAnchor="end" y={size - margin +3} 
@@ -359,9 +371,11 @@ const App = () => {
                 );
             })}
         </svg>
+        <p>This graph shows the comparison between for the video game of Genre sports sales (red) and all Genre's Video Games' sales(blue) in Euorope. The maximum sales amount of sports genre Video Games (in millions) in Euorope is 26.84 and the minimum sales amount (in millions) is 1.2. The graph shows the distribution of sales amount of sports genre Video Games is evenly distributed, and has a larger median than all Genre's Video Games' sales distribution. It shows in Euorope, sport genre video is more popular in all genres</p>
 
 
-        <h4>3.2 Platform</h4>
+
+        <h4>3.2 Highlighted Distributions of Sports genre Video Games' sales (in millions) in Japan</h4>
         <svg width={size} height={size} style={{border: "1px solid black"}}>
             
             <text x={size / 2 - 20} textAnchor="end" y={size - margin +3} 
@@ -424,10 +438,11 @@ const App = () => {
                 );
             })}
         </svg>
-    
-        <h3>4. Year</h3>
-        <h4>4.1 sports</h4>
+        <p>This graph shows the comparison between for the video game of Genre sports sales (red) and all Genre's Video Games' sales(blue) in Japan. The maximum sales amount of sports genre Video Games (in millions) in Euorope is 4.54and the minimum sales amount (in millions) is 0. The graph shows the distribution of sales amount of sports genre Video Games is more right skewed than all Genre's Video Games' sales distribution. It shows that sport genre video game is not as popuplar as other genres in Japan</p>
 
+    
+        <h3>4. What are the distributions of sales (in millions) for video games in the Year 2002 compared to the global game's sales in the all-time period(1980-2020)?</h3>
+        <h4>4.1 Highlighted Distributions of Video Games' sales (in millions) in the Year 2002 in Global</h4>
         <svg width={size} height={size} style={{border: "1px solid black"}}>
             <text x={size / 2 - 20} textAnchor="end" y={size - margin - 36} 
             style={{fontSize: 10, fontFamily: "italic"}}
@@ -490,6 +505,19 @@ const App = () => {
                 );
             })}
         </svg>
+        <p>This graph shows the distributions of sales (in millions) for video games in the Year 2002 (red) compared to the global game's sales (blue) in the all-time period(1980-2020). The maximum sales amount of the Year 2002 Video Games (in millions) in global wild is 37.28 and the minimum sales amount (in millions) is 1.2.  The maximum sales amount of all-time period(1980-2020) of Video Games (in millions) in global wild is 87.24 and the minimum sales amount (in millions) is 0. The graph shows two peaks of the distribution of sales amount in Year 2002. It may occur because few video games are especially popular than others. </p>
+
+
+        <h2>Paragraph 3: Analysis Process</h2>
+        <p>The first step I did to analyze this dataset is to look for the introduction on Kaggle and find the basic information about this dataset, such as each column's meaning, and after that I have a basic understanding of this. The second step I did is to clean the dataset to avoid null value. Then I sliced this dataset and used 300 rows of it, since the original file is to big to load. Based on my understanding and the research questions I came up, I decided to focus on several columns, they are video games' platforms, genre, the year of release, and sales amount in different regions. For the first question, I decided to only show the single barcode plot because it gives a nice overview of video games sales amount in different regions. For the 2, 3, 4 questions, I utilized highlight to point out the factor I want to focus on: the second set of graphs are same region, different platforms; the third one is same genre, different region; and the forth one is one specific year's sale amount compared with all time-period amount. </p>
+        <h2>Paragraph 4: Insight</h2>
+        <p>When I'm developing my exploration, I faced an issue which is I can't use "map" function. Then I found that the reason why I can't use is my dataset always return "nan" value. So I recleaned my data and replaced some "," to "." so that Visual Studio can know that it's a number instead of a string. From here I learned that we have to make sure the dataset is clean enough before we start to analyze it because data formats can cause many problems. I brought up 4 analysis questions and the first the now is an overview for video games sales amount in different regions. And we can see that in all three regions,  North American, Euorope, Japan, has right skewed shape on the graph. And it may because Video Game is not a necessary and only few video games can sell a huge amount. The second question we can see that both PS2 and DS platform has similar shape with all platforms data, so the platform may not be a key factor of selling video game. The third question, we can see sport genre video game is really popular in Euorope because it has a high median and its shape is normally distributed. But less popular in Japan because it's super right skewed. And it may because Japanese tend to work out less than Euoropean people. The forth question is distributions of sales (in millions) for video games in the Year 2002 compared to the global game's sales in the all-time period(1980-2020). We can see two peaks of the distribution of sales amount in Year 2002. It may occur because few video games are especially popular in that year. </p>
+        <h3>feedback/improvement based on feedback</h3>
+        <ol>
+            <li>feedback I received: For the first question, use line instead of empty circles so the whole webpage is more delicate; add description at the bottom for each graph for users to understand it better; lower the transparency so users can see the distribution more clearly.</li>
+            <li>According to the feedback, I used barcode plot for the first question and lowered the opacity. I then moved the description for each graph to the bottom so the webpage is more delicate. </li>
+        </ol>
+
 
 
 
