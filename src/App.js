156a549a129fbbbb32b24acc5602c879a217162c
Carol Wang <xtwang0@uw.edu>
2021-04-29 18:21:16 -0700

    cleaned data

--- src/App.js
+++ src/App.js
@@ -6,13 +6,28 @@ import { extent, max, min, bin } from "d3-array";
 
 const App = () => {
     const [data, loading] = useFetch(
-        "https://raw.githubusercontent.com/CarolWang820/INFO474Assignment2/main/countries%20of%20the%20world.csv"
+        "https://raw.githubusercontent.com/CarolWang820/INFO474Assignment2/main/countries-of-the-world.csv"
     );
+    
+ 
+
+    const size = 500;
+    const margin = 20;
+
 
     return (
     <div>
         <h1>Exploratory Data Analysis Focus on World Population</h1>
+        <p>{loading && "Loading data!"}</p>
+        <h2>Introduction</h2>
+        <h3>Distribution of Population Dentisy for All Countries</h3>
 
+        <svg width={size} height={size} style={{border: "1px solid black"}}>
+            <circle cx={10} cy={10} r="3" />
+            {data.map((measurement, index) => {
+                return <circle key={index} cx={10} cy={measurement.TMAX} r="3"/>;
+            })}
+        </svg>
     </div>
     );
 };
